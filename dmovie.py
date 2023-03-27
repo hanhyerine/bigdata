96f2a928d283cb7242ae0ebd3d33d6ea80c46269
han hyerin <flsdl3779@gmail.com>
2023-03-27 07:21:17 +0000

    평점 예매율개봉 추가

--- dmovie.py
+++ dmovie.py
@@ -2,24 +2,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.daum.net/ranking/reservation',headers=headers)
+# 다음 영화 순위 페이지 URL
+url = "https://movie.daum.net/ranking/reservation"
 
-soup = BeautifulSoup(data.text, 'html.parser')
-# print(soup)
-# 내가 원하는 위치에 있는 데이터 가져오기
-'''
-여기는 여러줄로 설명할수 있어요
-또 써도 되요
-'''
-#title = soup.select_one('#mainContent > div > div.box_ranking > ol > li:nth-child(1) > td.title > div > a')
-#print(title.text)
+# HTTP 요청 보내기
+response = requests.get(url)
 
-# #old_content > table > tbody > tr
-
-trs = soup.select("#old_content > table > tbody > tr")
-# print(trs)
-# 랭킹을 달아주기 위한 변수
+# HTTP 요청이 성공했는지 확인하기
+if response.status_code == 200:
+    # HTML 파싱하기
+    soup = BeautifulSoup(response.text, "html.parser")
+    # 영화 순위 리스트 찾기
+    rank = 0
+    movie_list = soup.select(".tit_item")
+    for tr in movie_list:
+        a_tag = tr.select_one("a")
+        rank = rank + 1
+        print(f'{rank}위 {a_tag.text}')
+else:
+    print("HTTP 요청 실패")
 rank = 0
 for tr in trs:
     a_tag = tr.select_one("td.title > div > a")
