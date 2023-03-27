3244b1c5345a562d2cab256395ec77b73207c90f
han hyerin <flsdl3779@gmail.com>
2023-03-27 07:22:21 +0000

    영화 평점 개봉 순위

--- dmovie.py
+++ dmovie.py
@@ -1,13 +1,10 @@
 # 다음 영화 순위 크롤링 하는 프로그램
 import requests
 from bs4 import BeautifulSoup
-
 # 다음 영화 순위 페이지 URL
 url = "https://movie.daum.net/ranking/reservation"
-
 # HTTP 요청 보내기
 response = requests.get(url)
-
 # HTTP 요청이 성공했는지 확인하기
 if response.status_code == 200:
     # HTML 파싱하기
@@ -15,16 +12,17 @@ if response.status_code == 200:
     # 영화 순위 리스트 찾기
     rank = 0
     movie_list = soup.select(".tit_item")
+    movie_list = soup.select(".thumb_cont")
     for tr in movie_list:
         a_tag = tr.select_one("a")
         rank = rank + 1
+        a_tag = tr.select_one("a")
         print(f'{rank}위 {a_tag.text}')
+        txt_grade = tr.select_one("span.txt_grade")
+        print(f'평점: {txt_grade.text}')
+        txt_num = tr.select_one("span.txt_num")
+        print(f'예매율: {txt_num.text}')
+        txt_date = tr.select_one(".txt_info > span.txt_num")
+        print(f'개봉날짜: {txt_date.text}')
 else:
     print("HTTP 요청 실패")
-rank = 0
-for tr in trs:
-    a_tag = tr.select_one("td.title > div > a")
-    # 만약에 a_tag 가 None이 아니면 text를 출력
-    if a_tag is not None:
-        rank = rank + 1
-        print(rank, a_tag.text)
