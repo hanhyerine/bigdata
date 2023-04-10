fc18ee9c3465794121e827fd07283e238a333c0a
han hyerin <flsdl3779@gmail.com>
2023-04-10 07:28:23 +0000

    힘든 함수였다

--- dmovie.py
+++ dmovie.py
@@ -2,6 +2,10 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+
+#내가 만든 모듈(함수)를 포함
+# import func
+from func import my_print
 # 다음 영화 순위 페이지 URL
 url = "https://movie.daum.net/ranking/reservation"
 # HTTP 요청 보내기
@@ -19,12 +23,12 @@ if response.status_code == 200:
         a_tag = tr.select_one("a")
         rank = rank + 1
         a_tag = tr.select_one("a")
-        print(f'{rank}위 {a_tag.text}')
+        my_print(f'{rank}위 {a_tag.text}')
         txt_grade = tr.select_one("span.txt_grade")
-        print(f'평점: {txt_grade.text}')
+        my_print(f'평점: {txt_grade.text}')
         txt_num = tr.select_one("span.txt_num")
-        print(f'예매율: {txt_num.text}')
+        my_print(f'예매율: {txt_num.text}')
         txt_date = tr.select_one(".txt_info > span.txt_num")
-        print(f'개봉날짜: {txt_date.text}')
+        my_print(f'개봉날짜: {txt_date.text}')
 else:
     print("HTTP 요청 실패")
---
+++ func.py
@@ -0,0 +1,4 @@
+def my_print(msg):
+    print('한혜린:', msg)
+
+# my_print('안녕')
